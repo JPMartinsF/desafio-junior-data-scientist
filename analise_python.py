9791a0a433f5e2b57d1f8e299d41d2b909789126
JoaoPedro <jpmartinsf27@gmail.com>
2025-03-19 21:48:37 -0300

    [feat] Modificado o arquivo para usar pandas até a questão 7

--- analise_python.py
+++ analise_python.py
@@ -1,108 +1,80 @@
 import basedosdados as bd
 from datetime import date
 import os
+import pandas as pd
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "desafiojuniordatascientist-5c2a920c3038.json"
 
 # Definição do projeto de faturamento
 billing_project_id = "desafiojuniordatascientist"
 
 # 1. Quantos chamados foram abertos no dia 01/04/2023?
-query_1 = """
-SELECT COUNT(*) AS total_chamados
+query_chamados = """
+SELECT id_chamado, data_inicio, id_bairro, tipo
 FROM `datario.adm_central_atendimento_1746.chamado`
 WHERE data_inicio >= '2023-04-01 00:00:00' 
 AND data_inicio < '2023-04-02 00:00:00'
 """
-df_1 = bd.read_sql(query_1, billing_project_id=billing_project_id)
-total_chamados = df_1.iloc[0, 0]
+df_chamados = bd.read_sql(query_chamados, billing_project_id=billing_project_id)
+total_chamados = df_chamados.shape[0]
 print(f"\n1. Total de chamados abertos em 01/04/2023: {total_chamados}")
 
 # 2. Qual o tipo de chamado que teve mais ocorrências nesse dia?
-query_2 = """
-SELECT tipo, COUNT(*) AS total_chamados
-FROM `datario.adm_central_atendimento_1746.chamado`
-WHERE data_inicio >= '2023-04-01 00:00:00' 
-AND data_inicio < '2023-04-02 00:00:00'
-GROUP BY tipo
-ORDER BY total_chamados DESC
-LIMIT 1
-"""
-df_2 = bd.read_sql(query_2, billing_project_id=billing_project_id)
+df_2 = df_chamados.groupby("tipo").agg({"id_chamado": "count"}).reset_index()
+df_2.sort_values("id_chamado", ascending=False, inplace=True)
 tipo_mais_frequente, qtd_tipo = df_2.iloc[0]
 print(f"\n2. Tipo de chamado mais frequente: {tipo_mais_frequente} ({qtd_tipo} chamados)")
 
 # 3. Quais os 3 bairros com mais chamados?
-query_3 = """
-SELECT 
-  b.nome AS nome_bairro, 
-  COUNT(c.id_bairro) AS total_chamados
-FROM `datario.adm_central_atendimento_1746.chamado` AS c
-JOIN `datario.dados_mestres.bairro` AS b
-  ON c.id_bairro = b.id_bairro
-WHERE c.data_inicio >= '2023-04-01 00:00:00' 
-AND c.data_inicio < '2023-04-02 00:00:00'
-GROUP BY b.nome
-ORDER BY total_chamados DESC
-LIMIT 3
+query_bairros = """
+SELECT id_bairro, nome, subprefeitura
+FROM `datario.dados_mestres.bairro`
 """
-df_3 = bd.read_sql(query_3, billing_project_id=billing_project_id)
+df_bairros = bd.read_sql(query_bairros, billing_project_id=billing_project_id)
+df_chamados_bairro = df_chamados.merge(df_bairros, on="id_bairro", how="left")
+df_3 = df_chamados_bairro.groupby("nome").agg({"id_chamado": "count"}).reset_index()
+df_3.sort_values("id_chamado", ascending=False, inplace=True)
 print("\n3. Top 3 bairros com mais chamados:")
-print(df_3.to_string(index=False))
+print(df_3.iloc[0:3].to_string(index=False))
 
 # 4. Subprefeitura com mais chamados
-query_4 = """
-SELECT b.subprefeitura AS nome_subprefeitura, COUNT(*) AS total_chamados
-FROM `datario.adm_central_atendimento_1746.chamado` AS c
-LEFT JOIN `datario.dados_mestres.bairro` AS b ON c.id_bairro = b.id_bairro
-WHERE c.data_inicio >= '2023-04-01 00:00:00' 
-AND c.data_inicio < '2023-04-02 00:00:00'
-GROUP BY nome_subprefeitura
-ORDER BY total_chamados DESC
-LIMIT 1
-"""
-df_4 = bd.read_sql(query_4, billing_project_id=billing_project_id)
+df_4 = df_chamados_bairro.groupby("subprefeitura").agg({"id_chamado": "count"}).reset_index()
+df_4.sort_values("id_chamado", ascending=False, inplace=True)
 nome_subprefeitura, qtd_subprefeitura = df_4.iloc[0]
 print(f"\n4. Subprefeitura com mais chamados: {nome_subprefeitura} ({qtd_subprefeitura} chamados)")
 
 # 5. Quantidade de chamados sem bairro associado
-query_5 = """
-SELECT COUNT(*) AS total_chamados_sem_bairro
-FROM `datario.adm_central_atendimento_1746.chamado`
-WHERE data_inicio >= '2023-04-01 00:00:00' 
-AND data_inicio < '2023-04-02 00:00:00'
-AND id_bairro IS NULL
-"""
-df_5 = bd.read_sql(query_5, billing_project_id=billing_project_id)
-total_sem_bairro = df_5.iloc[0, 0]
+df_5 = df_chamados_bairro[df_chamados_bairro["id_bairro"].isna()]
+total_sem_bairro = df_5.shape[0]
 print(f"\n5. Chamados sem bairro associado: {total_sem_bairro}")
 
 # 6. Quantidade de chamados do subtipo "Perturbação do sossego" em 2022 e 2023
 # "Perturbação do sossego" aparece apenas na coluna de tipos
 query_6 = """
-SELECT COUNT(*) AS total_chamados
+SELECT id_chamado, data_inicio, tipo, subtipo
 FROM `datario.adm_central_atendimento_1746.chamado`
 WHERE tipo = "Perturbação do sossego"
 AND data_inicio BETWEEN '2022-01-01' AND '2023-12-31'
 """
-df_6 = bd.read_sql(query_6, billing_project_id=billing_project_id)
-total_chamados_perturbacao = df_6.iloc[0,0]
+df_chamados_22_23 = bd.read_sql(query_6, billing_project_id=billing_project_id)
+df_chamados_22_23.drop(["tipo", "subtipo"], axis=1, inplace=True)
+total_chamados_perturbacao = df_chamados_22_23.shape[0]
 print(f"\n6. Chamados com o subtipo 'Perturbação do sossego': {total_chamados_perturbacao}")
 
 # 7. Chamados abertos durante eventos
-query_7 = """
-SELECT 
-    c.id_chamado,
-    c.data_inicio AS data_chamado,
-    e.evento
-FROM `datario.adm_central_atendimento_1746.chamado` AS c
-JOIN `datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos` AS e
-    ON c.data_inicio BETWEEN e.data_inicial AND e.data_final
-WHERE c.tipo = "Perturbação do sossego"
-AND e.data_inicial IS NOT NULL
-AND e.data_final IS NOT NULL
-ORDER BY c.data_inicio ASC;
+query_eventos = """
+SELECT data_inicial, data_final, evento
+FROM `datario.turismo_fluxo_visitantes.rede_hoteleira_ocupacao_eventos`
 """
-df_7 = bd.read_sql(query_7, billing_project_id=billing_project_id)
+df_eventos = bd.read_sql(query_eventos, billing_project_id=billing_project_id)
+df_eventos = df_eventos.dropna(subset=["data_inicial", "data_final"])
+df_eventos["data_inicial"] = pd.to_datetime(df_eventos["data_inicial"])
+df_eventos["data_final"] = pd.to_datetime(df_eventos["data_final"])
+df_merged = df_chamados_22_23.merge(df_eventos, how="cross")
+df_7 = df_merged[
+    (df_merged["data_inicio"] >= df_merged["data_inicial"]) &
+    (df_merged["data_inicio"] <= df_merged["data_final"])
+]
+
 print(f"\n7. Chamados abertos durante os eventos do tipo 'perturbação do sossego': \n{df_7.head(5)}")
 
 # 8. Quantidade de chamados do tipo abertos em cada evento
