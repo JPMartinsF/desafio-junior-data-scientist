5d5f95615928ab2b0b811b0a3b26000cab20c4be
JoaoPedro <jpmartinsf27@gmail.com>
2025-03-20 19:26:05 -0300

    [feat] Respondidas as questões 8 e 9

--- analise_api.py
+++ analise_api.py
@@ -19,26 +19,26 @@ try:
 
     if (resposta_feriados.status_code == 200) and (resposta_clima.status_code == 200):
         feriados = resposta_feriados.json()
-        print(f"1. O número de feriados no Brasil em 2024 é: {len(feriados)}")
+        print(f"\n1. O número de feriados no Brasil em 2024 é: {len(feriados)}")
 
         meses = [int(feriado["date"].split("-")[1]) for feriado in feriados]
         contador_meses = Counter(meses)
         mes_mais_feriados = max(contador_meses, key=contador_meses.get)
-        print(f"2. O mês com mais feriados é {meses_mapper[mes_mais_feriados]}")
+        print(f"\n2. O mês com mais feriados é {meses_mapper[mes_mais_feriados]}")
 
         feriados_dias_uteis = 0
         for feriado in feriados:
             data = datetime.strptime(feriado["date"], "%Y-%m-%d")
             if data.weekday() < 5:
                 feriados_dias_uteis += 1
-        print(f"3. O número de feriados de 2024 que caem em dias úteis é {feriados_dias_uteis}")
+        print(f"\n3. O número de feriados de 2024 que caem em dias úteis é {feriados_dias_uteis}")
 
         dados_clima = resposta_clima.json()["daily"]
         df_clima = pd.DataFrame(dados_clima)
         df_clima["time"] = pd.to_datetime(df_clima["time"])
         df_clima["mes"] = df_clima["time"].dt.month
         grouped_df_clima = df_clima.groupby("mes").mean("temperature_2m_mean").reset_index()
-        print("4. Temperatura média de cada mês: ")
+        print("\n4. Temperatura média de cada mês: ")
         print(grouped_df_clima[["mes", "temperature_2m_mean"]].to_string(index=False))
 
         tempo_predominante = df_clima.groupby("mes")["weather_code"].agg(lambda x: x.mode()[0]).reset_index()
@@ -46,18 +46,25 @@ try:
             weather_code_dict = json.load(file)
         weather_code_mapping = {int(code): data["day"]["description"] for code, data in weather_code_dict.items()}
         tempo_predominante["weather_description"] = tempo_predominante["weather_code"].map(weather_code_mapping)
-        print("5. O tempo predominante de cada mês foi: ")
+        print("\n5. O tempo predominante de cada mês foi: ")
         print(tempo_predominante[["mes", "weather_description"]].to_string(index=False))
 
         df_feriados = pd.DataFrame(feriados)
         df_feriados["date"] = pd.to_datetime(df_feriados["date"])
         merged_df = df_clima.merge(df_feriados[["date", "localName"]], left_on="time", right_on="date")
         merged_df["weather_description"] = merged_df["weather_code"].map(weather_code_mapping)
-        print("6. O tempo e temperatura média de cada feriado foi:")
-        print(merged_df[["localName", "date", "weather_description", "temperature_2m_mean"]])
+        print("\n6. O tempo e temperatura média de cada feriado foi:")
+        print(merged_df[["localName", "date", "weather_description", "temperature_2m_mean"]].to_string(index=False))
 
-        True
-    else:
-        print(f"Erro na requisição: {resposta_feriados.status_code}")
+        tempos_bons = [0, 1, 2]
+        feriados_ruins = merged_df[~merged_df["weather_code"].isin(tempos_bons)]
+        print(f"\n7.Houveram {feriados_ruins.shape[0]} feriados ruins, foram eles:")
+        print(feriados_ruins[["localName", "date"]].to_string(index=False))
+
+        feriados_bons = merged_df[merged_df["weather_code"].isin(tempos_bons)]
+        melhor_feriado = feriados_bons.loc[feriados_bons["temperature_2m_mean"].idxmax()]
+        print(f"\n8. O feriados 'mais aproveitável' de 2024 foi o {melhor_feriado['localName']} \n   em {melhor_feriado['date']}", end="")
+        print(f"com temperatura de {melhor_feriado['temperature_2m_mean']}°C e clima {melhor_feriado['weather_description']}")
+
 except:
     print("Erro na requisição")
